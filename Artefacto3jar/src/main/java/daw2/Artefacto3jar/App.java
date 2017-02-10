81c60a894318c98b159115f4b4dbcd5c9a18c316
dwes <dwes@PROFESORTI3>
2017-02-10 09:29:15 +0100

    subida 1

---
+++ Artefacto3jar/src/main/java/daw2/Artefacto3jar/App.java
@@ -0,0 +1,13 @@
+package daw2.Artefacto3jar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
